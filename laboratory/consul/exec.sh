9c7ff45d7045542ffea0f112116d4bb1da3c21c6
Willian Vieira Cardoso <wvcardoso@gmail.com>
2017-10-23 01:21:42 -0200

    last version of simulate environment consul

--- laboratory/consul/exec.sh
+++ laboratory/consul/exec.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 consul='consul'
 
@@ -9,12 +9,8 @@ docker run -d -p 8500:8500 --name $consul -h $consul progrium/consul -server -bo
 JOIN_IP="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' $consul)"
 
 ### SIMULATE ONE CONSUL AGENT MODE
-docker run -d --name node1 -h node1 -v $PWD/node1.json:/config/consul.json  progrium/consul -join $JOIN_IP
-docker run -d --name node2 -h node1 -v $PWD/node2.json:/config/consul.json  progrium/consul -join $JOIN_IP
-docker run -d --name node3 -h node1 -v $PWD/node3.json:/config/consul.json  progrium/consul -join $JOIN_IP
 
-### SIMULATE ONE SERVICE
-docker run -d --name apache1 nimmis/alpine-apache
-docker run -d --name apache2 nimmis/alpine-apache
-docker run -d --name apache3 nimmis/alpine-apache
-
+for i in $(seq 1 3); do
+  docker run -d --name node${i} -h node${i} -v $PWD/node${i}.json:/config/consul.json  progrium/consul -join $JOIN_IP
+  docker run -d --name apache${i} nimmis/alpine-apache
+done
